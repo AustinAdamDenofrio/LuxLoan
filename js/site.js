3f2287dd12e6aae31da2872f162f4cfb1363c984
Austin Denofrio DT <denofrioaustin@gmail.com>
2024-04-16 20:34:34 -0400

    framwork build for js app. Fixing bugs

--- js/site.js
+++ js/site.js
@@ -21,7 +21,7 @@ function getValues() {
     let statements = generateLoanInformation(loanAmount, termAmount, interestRate, totalMonthlyPayment);
 
     // Write the values to html so the user can see
-    displayLoanInformation(statements, termAmount);
+    displayLoanInformation(statements);
 
     
     
@@ -33,7 +33,7 @@ function  generateTotalMonthlyPayments(loanAmount, termAmount, interestRate) {
     // let termExponant = -Math.abs(termAmount);
 
     // Total Monthly Payment = (loanAmount) * (interestRate / 1200) / ( 1 - (1 + interestRate/1200)^(-termAmount) )
-    let results = loanAmount * (interestRate/1200) / (1 - Math.pow((1 + interestRate / 1200), -Math.abs(termAmount)));
+    let results = loanAmount * (interestRate/1200) / (1 - Math.pow((1 + interestRate / 1200), -termAmount));
 
     return results;
 }
@@ -42,38 +42,40 @@ function  generateTotalMonthlyPayments(loanAmount, termAmount, interestRate) {
 // Each month should be an object, maybe called statement, with a property for each col.
 // build an array of bills that can be used to generate a table.
  function generateLoanInformation(loanAmount, termAmount, interestRate, totalMonthlyPayment){
+     
+        // Month
+    let loanLength = termAmount;
+       // Payment
     const payment = totalMonthlyPayment;
-    const loanLength = termAmount;
+        // Balance (initializes as = lounAmount and will change as person pays)
+    let remainingBalance = loanAmount;
+        // Total interest (Total paid interest)
+    let totalInterestPayment = 0;    
+       // create and array and use for loop to build an array
+    let loanTable = [];    
+    
+    for (let i = 0; i < loanLength; i++){        
+            // Interest
+        let interest = remainingBalance * interestRate/1200;
+        totalInterestPayment += interest
+            // Principal    
+        let principalPayment = payment - interest;
+        remainingBalance -= principalPayment;
 
-    let remainingBalance = 0;
-    let interestPayment = 0;
-    let principalPayment = 0;
-    
-    // create and array and use for loop to build an array
-    let loanTable = [];
-    
-    
-    for (let i = 0; i < termAmount.length; i++){
-        
-        remainingBalance = loanAmount;
-        interestPayment = 0;
-        principalPayment = payment - interestPayment;
-
-        // assign the values of each table collumn into an object
-        const bill = {
-            month: i++,
+        // assign the values of each table column into an object
+        loanTable.push({
+            month: i + 1,
             payment: payment,
             principal: principalPayment,
-            interest: interestPayment,
-            total_interest: interestPayment + "the interest payment from before" ,
-            balance: loanAmount - remainingBalance
-        };
+            interest: interest,
+            total_interest: totalInterestPayment,
+            balance: remainingBalance
+        });
 
-        loanTable.push(bill);
         // change the values that change month to month
 
         // build an array of those objects
-    }
+    };
 
     return loanTable;
 
@@ -81,16 +83,33 @@ function  generateTotalMonthlyPayments(loanAmount, termAmount, interestRate) {
  }
     
     
-function displayLoanInformation(statements, termAmount){
-
+function displayLoanInformation(statements){
     // FOREACH object in the array, target every property value and assign to var. 
     // use that var to create new html table that has rows = number of objects in the array
 
-    for (let i = 0;i < termAmount.length) {
+    // statements.forEach((statement, i) => {
+
+    // })
+    let tbody = document.getElementById('table-content');
+    let tableHtml = '';
 
 
+    for (let i = 0; i < statements.length;i++) {
+
+        let statement = statements[i];
+        let month = statement.month;
+        let payment = statement.payment; 
+        let principal = statement.principal;
+        let interest = statement.interest;
+        let totalInterest = statement.total_interest;
+        let balance = statement.balance; 
+        
+    tableHtml += `<tr><td>${month}</td><td>${payment}</td><td>${principal}</td><td>${interest}</td><td>${totalInterest}</td><td>${balance}</td></tr>`;
     }
 
+    tbody.innerHTML = tableHtml;
 }
+
+
     
   
